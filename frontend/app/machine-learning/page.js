01ccba36e09185424770b1cadc93f8a27fb9a75d
jetxshen <jcho0161@student.monash.edu>
2025-04-24 16:06:35 +0800

    Created frontend first draft, along with leaflet js map.

---
+++ frontend/app/machine-learning/page.js
@@ -0,0 +1,8 @@
+export default function DataPage() {
+    return (
+      <div className="bg-white dark:bg-gray-800 rounded-lg shadow p-6">
+        <h1 className="text-2xl font-bold">Machine Learning</h1>
+        <p>Soon to implement Machine Learning!</p>
+      </div>
+    );
+  }
---
+++ frontend/app/settings/page.js
@@ -0,0 +1,8 @@
+export default function DataPage() {
+    return (
+      <div className="bg-white dark:bg-gray-800 rounded-lg shadow p-6">
+        <h1 className="text-2xl font-bold">Settings</h1>
+        <p>Soon to implement Settings!</p>
+      </div>
+    );
+  }
